088ebfe0a2520d07a3a8ac5316ac10c71aa1df66
mkhalegaonkar3 <mkhalegaonkar3@gmail.com>
2019-09-08 01:15:00 +0530

    added API calls

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -11,12 +12,20 @@ import (
 
 var db *gorm.DB
 
-type product struct {
-	gorm.Model
-	ProductName     string `json:"product_name"`
-	ProductQuantity int    `json:"product_quantity"`
-	ProductPrice    int    `json:"product_price"`
-}
+type (
+	product struct {
+		gorm.Model
+		ProductName     string `json:"product_name"`
+		ProductQuantity int    `json:"product_quantity"`
+		ProductPrice    int    `json:"product_price"`
+	}
+	transformedProduct struct {
+		ProductID       uint   `json:"id"`
+		ProductName     string `json:"product_name"`
+		ProductQuantity int    `json:"product_quantity"`
+		ProductPrice    int    `json:"product_price"`
+	}
+)
 
 func init() {
 	var err error
@@ -31,11 +40,12 @@ func init() {
 func main() {
 
 	router := gin.Default()
+	router.Use(static.Serve("/", static.LocalFile("./view", true)))
 
 	v1 := router.Group("/api/v1/products")
 
 	v1.POST("/", addProduct)
-	// v1.GET("/", getProducts)
+	v1.GET("/", getProducts)
 
 	router.Run()
 }
@@ -56,6 +66,20 @@ func addProduct(c *gin.Context) {
 
 }
 
-// func getProducts(c *gin.Context) {
+func getProducts(c *gin.Context) {
+	var products []product
+	var _products []transformedProduct
 
-// }
+	db.Find(&products)
+	if len(products) <= 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No products found !!"})
+		return
+	}
+	for _, item := range products {
+		_products = append(_products, transformedProduct{ProductID: item.ID, ProductName: item.ProductName, ProductQuantity: item.ProductQuantity, ProductPrice: item.ProductPrice})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"data":   _products,
+	})
+}
